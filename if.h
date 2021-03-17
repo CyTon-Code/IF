e18f3ab09443f604e305144540c3f78dcbd46c4c
CyTon Program Languages <cytoncode@gmail.com>
2021-03-17 12:09:45 +0200

    Update if.h

--- if.h
+++ if.h
@@ -2,13 +2,17 @@
 #define if_construction
 
 void if_true(){
+    cout << "1 < 2 is true";
 }
 void if_false(){
+    cout << "1 < 2 is false";
 }
 
 void IF(bool term){
-  void (*_false)() = if_false*int(term);
-  void (*_true)() = if_true*int(!term);
+  void (*_false)();
+  void (*_true)();
+  _false = if_false*term;
+  _true = if_true*!term;
   _true();_false();
 }
 
