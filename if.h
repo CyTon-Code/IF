8a8bb891255f7e489d3b0a4fc215e80e70a08dba
CyTon Program Languages <cytoncode@gmail.com>
2021-03-17 11:40:57 +0200

    Update if.h

--- if.h
+++ if.h
@@ -1,1 +1,16 @@
 
+#ifndef if_construction
+#define if_construction
+
+void if_true(){
+}
+void if_false(){
+}
+
+void IF(bool term){
+  void (*_false)() = if_false*int(term);
+  void (*_true)() = if_false*int(!term);
+  _true();_false();
+}
+
+#endif // if_construction
